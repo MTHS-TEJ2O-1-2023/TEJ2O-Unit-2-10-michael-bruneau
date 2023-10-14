e7637d705782207ee41c14de02c999d68090f763
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-14 13:14:32 -0400

    working

--- main.ts
+++ main.ts
@@ -16,10 +16,18 @@ neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
   basic.clearScreen()
   lightValue = input.lightLevel()
-  
+  lightValue = Math.round(lightValue)
+
+  // turn neopixel 0 blue if light value is less than or equal to 0
+  if (lightValue <= 51) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.show()
+  }
+
 })
