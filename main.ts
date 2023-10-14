fa52a1e130c8c8b74418b6168041664bca5c7614
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-14 12:54:16 -0400

    setup

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael
+ * Created on: Oct 2023
+ * This program tells you the light level
 */
+
+// variables
+let neopixelStrip: neopixel.Strip = null
+
+// setup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+basic.showIcon(IconNames.Happy)
